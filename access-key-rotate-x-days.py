8d950c8c47ebabe74f8c6a7664f9399d6cff3a3a
SrinathK1 <srinath.krishnamoorthy@searce.com>
2021-11-25 11:55:12 +0530

    access-key-new-version

--- access-key-rotate-x-days.py
+++ access-key-rotate-x-days.py
@@ -10,10 +10,17 @@ globalVars['Region_Name'] = "ap-south-1"
 def get_usr_old_keys(event, context):
 
     client = boto3.client('iam',region_name = globalVars['Region_Name'])
+    #snsClient = boto3.client('sns',region_name = globalVars['Region_Name'])
+    #ses = boto3.client('ses')
     paginator = client.get_paginator('list_users')
     CurrentDate = datetime.now(timezone.utc)
     MaxKeyAge = 30
-
+    sec_client = boto3.client('secretsmanager')
+    response1 = sec_client.get_secret_value(SecretId='lambda/AccessKeyRotate/password')
+    sec_dict = json.loads(response1['SecretString'])
+    sec_email = sec_dict['Email']
+    sec_password = sec_dict['Password']
+    
     for response in paginator.paginate():
         for user in response['Users']:
             user_name = user['UserName']
@@ -24,23 +31,20 @@ def get_usr_old_keys(event, context):
                 age = (CurrentDate - key_creation_date).days
                 if age > MaxKeyAge:
                     print("Access key Expired for : ", user_name)
-                    gmail_user = <e-mail>
-                    gmail_password = <Password>
-                    sent_from = <e-mail>
-                    sent_to = <e-mail>
+                    gmail_user = sec_email
+                    sent_from = sec_email
+                    sent_to = user_name
                     sent_sub = "Access key Expired!!"
-                    sent_body = "Dear User -  Your Aceess key Expired. Please create new one."
+                    sent_body = "Dear User -  Your Access key Expired. Please create new one."
                     try:
                         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
                         server.ehlo()
-                        server.login(gmail_user, gmail_password)
+                        server.login(gmail_user, sec_password)
                         server.sendmail(sent_from, sent_to, sent_body)
-                        #server.close()
-                        print(email_text)
+                        server.close()
                         print('Email sent!')
                     except Exception as exception:
                         print("Error: %s!\n\n" % exception)
-
                     
 def lambda_handler(event, context):
     get_usr_old_keys(event,context)
